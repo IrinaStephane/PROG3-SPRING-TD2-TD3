0a6d31f049d1b7fac1bbaaaa0744f9745baf4bb4
Irina <irinarakoto2005@gmail.com>
2026-03-19 22:17:38 +0300

    chore: initialize project

---
+++ src/main/java/school/hei/td2/Prog3SpringTd2Td3Application.java
@@ -0,0 +1,13 @@
+package school.hei.td2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3SpringTd2Td3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3SpringTd2Td3Application.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/td2/Prog3SpringTd2Td3ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.td2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringTd2Td3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
